7c1d86ec168c52a925ea6ff0ea6d17468c436a2f
Varga-Somogyi Ákos <vsakos@gmail.com>
2023-08-15 12:53:38 +0200

    Update initialize.ts

--- src/initialize.ts
+++ src/initialize.ts
@@ -4,11 +4,25 @@ import Solflare from '@solflare-wallet/metamask-sdk';
 import detectEthereumProvider from '@metamask/detect-provider';
 import { getMetamaskProvider, isSnapSupported, MetaMaskEthereumProvider } from './provider';
 
+let isInitialized = false;
+
 export function initialize(instance?: Solflare): void {
+  if (isInitialized) {
+    return;
+  }
+
+  isInitialized = true;
+
   registerWallet(new SolflareMetamaskWallet(instance ?? new Solflare()));
 }
 
 export async function initializeWhenDetected(instance?: Solflare): Promise<void> {
+  if (isInitialized) {
+    return;
+  }
+
+  isInitialized = true;
+
   const provider = await detectEthereumProvider({ silent: true });
 
   if (!provider) {
