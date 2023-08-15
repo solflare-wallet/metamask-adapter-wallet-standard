23666c99c0554ac0889fe2e98fc6af5f08604c73
Varga-Somogyi Ákos <vsakos@users.noreply.github.com>
2023-08-15 12:49:40 +0200

    initializeWhenDetected (#1)
    * initializeWhenDetected
    
    * Wrap in try-catch

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,7 +1,31 @@
 import { registerWallet } from './register.js';
 import { SolflareMetamaskWallet } from './wallet.js';
 import Solflare from '@solflare-wallet/metamask-sdk';
+import detectEthereumProvider from '@metamask/detect-provider';
+import { getMetamaskProvider, isSnapSupported, MetaMaskEthereumProvider } from './provider';
 
 export function initialize(instance?: Solflare): void {
   registerWallet(new SolflareMetamaskWallet(instance ?? new Solflare()));
+}
+
+export async function initializeWhenDetected(instance?: Solflare): Promise<void> {
+  const provider = await detectEthereumProvider({ silent: true });
+
+  if (!provider) {
+    return;
+  }
+
+  const metamaskProvider = getMetamaskProvider(provider as MetaMaskEthereumProvider);
+
+  if (!metamaskProvider) {
+    return;
+  }
+
+  const snapSupported = await isSnapSupported(metamaskProvider);
+
+  if (!snapSupported) {
+    return;
+  }
+
+  initialize(instance);
 }
